81bcc1a457994dd286c52923dd0e28d56956b999
DanielSAbrahms <ws-dabrahms@gonzaga.edu>
2021-02-27 13:06:02 -0800

    git added health and health bar

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth;
+    public int minHealth = 0;
+    public int currentHealth;
+    public bool hasHealth;
+
+    private HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        hasHealth = true;
+        healthBar.slider.maxValue = maxHealth;
+        healthBar.slider.minValue = minHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void giveHealth (int newHealth)
+    {
+        updateHealth(currentHealth + newHealth);
+    }
+
+    public void takeDamage(int incomingDamage)
+    {
+        updateHealth(currentHealth - incomingDamage);
+    }
+
+    private void updateHealth(int newHealth)
+    {
+        currentHealth = newHealth;
+
+        int surplusHealth = currentHealth - maxHealth;
+        if (surplusHealth > 0) currentHealth -= surplusHealth;
+
+        if (currentHealth <= 0)
+        {
+            hasHealth = false;
+            currentHealth = minHealth;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Health playerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth.maxHealth = 100;
+        playerHealth.minHealth = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
